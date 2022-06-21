1622bb7c425b32576982c737ce6e291c45894002
Jesse Smith <jesse.smith@stjude.org>
2022-06-21 13:23:50 -0500

    Refactor `ibindlist()` and add `last_seed_value` documentation

--- R/ibindlist.R
+++ R/ibindlist.R
@@ -4,7 +4,7 @@
 #' object. The resulting number of imputed data sets is equal to the sum of the
 #' number of imputed data sets in each list element.
 #'
-#' The `call` and `seed` arguments are primarily used by
+#' The `call`, `seed`, and `last_seed_value` arguments are primarily used by
 #' \code{\link[futuremice:future_mice]{future_mice()}} and
 #' \code{\link[futuremice:future_mids]{future_mids()}}; they allow modification of the
 #' `mids` object to match the equivalent \code{\link[mice:mice]{mice::mice()}}
@@ -23,7 +23,7 @@
 #'
 #' @export
 ibindlist <- function(mids_list, call = NULL, seed = NULL, last_seed_value = NULL) {
-  # Combine into single `mids` object
+  # Reduce to single `mids` object
   mids <- purrr::reduce(mids_list, mice::ibind)
 
   # Set `ignore` attribute
@@ -32,15 +32,47 @@ ibindlist <- function(mids_list, call = NULL, seed = NULL, last_seed_value = NUL
   # Set `seed` attribute
   if (!is.null(seed)) mids$seed <- mice_seed(seed)
 
+  # Set names of imputation data frames
+  mids$imp <- purrr::map(
+    mids$imp,
+    fm_set_colnames,
+    names = as.character(seq_len(mids$m))
+  )
+
   # Set `lastSeedValue` attribute
   if (!is.null(last_seed_value)) mids$lastSeedValue <- last_seed_value
 
   # Set `call` attribute
+  if (is.null(call)) call <- mids_list[[1L]]$call
+  mids <- fm_set_call(mids, call = call)
+
+  # Set `loggedEvents` attribute
+  mids <- fm_set_logged_events(mids, mids_list)
+
+  # Return
+  mids
+}
+
+
+#' Helper Function for Setting `mids$call`
+#'
+#' @param mids A `mids` object to update
+#' @param call A `call` object to use as `mids$call` after standardization. Can
+#'   be `NULL`, in which case `mids` is returned unaltered.
+#'
+#' @return The `mids` object with an updated `call` (if `call` is not `NULL`).
+#'   Note that if `call` has arguments `m`, `maxit`, or `seed`, their values
+#'   are updated to reflect the corresponding attribute in `mids`.
+#'
+#' @keywords internal
+fm_set_call <- function(mids, call) {
+  # Handle special cases
   if (is.null(call)) {
-    call <- mids_list[[1L]]$call
+    return(mids)
   } else if (!rlang::is_call(call)) {
     rlang::abort("`call` must be a call object or `NULL`")
   }
+  # Updates call arguments
   call_arg_nms <- c(
     rlang::call_args_names(call),
     rlang::fn_fmls_names(rlang::call_fn(call))
@@ -49,12 +81,31 @@ ibindlist <- function(mids_list, call = NULL, seed = NULL, last_seed_value = NUL
   if ("m" %in% call_arg_nms) call$m <- mids$m
   if ("maxit" %in% call_arg_nms) call$maxit <- mids$maxit
   if ("seed" %in% call_arg_nms && !is.null(seed)) call$seed <- mids$seed
+  # Set
   mids$call <- call
+  # Return
+  call
+}
 
-  # Set names of imputation data frames
-  nms <- as.character(seq_len(mids$m))
-  mids$imp <- purrr::map(mids$imp, fm_set_colnames, names = nms)
 
+#' Helper Function for Setting `mids$loggedEvents`
+#'
+#' @param mids A `mids` object to update
+#' @param mids_list A list of `mids` objects with `loggedEvents` to extract and
+#'   combine
+#'
+#' @return `mids` with `loggedEvents` from `mids_list`
+#'
+#' @keywords internal
+fm_set_logged_events <- function(mids, mids_list) {
+  # Combine loggedEvents from `mids_list`
+  loggedEvents <- purrr::map_dfr(mids_list, ~ .x$loggedEvents, .id = "i")
+  # Update if any exist
+  if (NROW(loggedEvents) > 0L) {
+    loggedEvents$im <- loggedEvents$im + as.integer(loggedEvents$i) - 1L
+    loggedEvents$i  <- NULL
+    mids$loggedEvents <- loggedEvents
+  }
   # Return
   mids
 }
