2c4f93f7f2a41cc838e7e627ea562e45b5f0c827
Jesse Smith <jesse.smith@stjude.org>
2022-06-28 12:50:36 -0500

    Update `future_mice()` snapshot test

--- tests/testthat/_snaps/future_mice.md
+++ tests/testthat/_snaps/future_mice.md
@@ -1,17 +1,17 @@
-# `future_mice()` works
+# `future_mice()` works locally
 
     Code
-      future_mice(mice::nhanes2, seed = 1L, maxit = 5L, minit = 1L)
+      future_mice(mice::nhanes, maxit = 5L, seed = 1L)
     Warning <rlang_warning>
       Sampling did not converge in 5 iterations
     Message <rlang_message>
-      R-hat: 2.498
+      R-hat: NA/NA/1.903/1.903
     Output
       Class: mids
       Number of multiple imputations:  5 
       Imputation methods:
-           age      bmi      hyp      chl 
-            ""    "pmm" "logreg"    "pmm" 
+        age   bmi   hyp   chl 
+         "" "pmm" "pmm" "pmm" 
       PredictorMatrix:
           age bmi hyp chl
       age   0   1   1   1
--- tests/testthat/test-future_mice.R
+++ tests/testthat/test-future_mice.R
@@ -1,6 +1,17 @@
-test_that("`future_mice()` works", {
+test_that("`future_mice()` works locally", {
   withr::local_seed(1L)
   future::plan("multisession", workers = pmin(2L, future::availableCores()))
-  expect_snapshot(future_mice(mice::nhanes2, seed = 1L, maxit = 5L, minit = 1L))
-  future::plan("sequential")
+  withr::defer(future::plan("sequential"))
+  is_inst <- furrr::future_map_lgl(
+    seq_len(4L),
+    function(x) rlang::is_installed("futuremice")
+  )
+  if (!all(is_inst)) {
+    tryCatch(
+      devtools::install(reload = FALSE, quick = TRUE, quiet = TRUE),
+      error = function(e) skip("Could not install `{futuremice}` in test env")
+    )
+    withr::defer(suppressMessages(remove.packages("futuremice")))
+  }
+  expect_snapshot(future_mice(mice::nhanes, maxit = 5L, seed = 1L))
 })
